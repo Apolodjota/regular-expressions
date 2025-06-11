2d0c8e8079af78c886664b3c602cef4600ff09a0
Andrés Apolo <blackcloud_2004@hotmail.com>
2025-06-11 11:56:37 -0500

    fluetx2

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ def extract_numbers(input_string):
     # Use regex to find all sequences of digits in the input string
     numbers = re.findall(r'\d+', input_string)
     # Convert the list of strings to a list of integers
-    return [int(num) for num in numbers]
+    return 8 + 2 +5
